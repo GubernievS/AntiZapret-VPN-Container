cb0bc78efbe2ba65c6be7df19243926ed40d3884
GubernievS <tyman@ya.ru>
2024-08-06 12:23:17 +0300

    Update antizapret-vpn.sh

--- antizapret-vpn.sh
+++ antizapret-vpn.sh
@@ -3,12 +3,12 @@
 # + Разблокирован YouTube и часть сайтов блокируемых без решения суда
 # Для увеличения скорости используется UDP и 443 порт для обхода блокировки по портам
 #
-# Версия 5.2 от 06.08.2024
+# Версия 5.3 от 06.08.2024
 # https://github.com/GubernievS/AntiZapret-VPN-Container
 #
-# Протестировано на Ubuntu 20.04   Процессор: 1 core   Память: 1 Gb   Хранилище: 10 Gb
+# Протестировано на Ubuntu 20.04 - Процессор: 1 core Память: 1 Gb Хранилище: 10 Gb
 #
-# 1. Установить на VDS Ubuntu 20.04 или новее
+# 1. Установить на VDS Ubuntu 20.04 (или новее если 2 Gb памяти)
 # 2. Загрузить этот файл на сервер в папку root по SFTP (например через программу FileZilla)
 # 3. В консоли под root выполнить:
 # chmod +x ./antizapret-vpn.sh && ./antizapret-vpn.sh
@@ -24,13 +24,16 @@
 # Изменить файл с личным списком антизапрета include-hosts-custom.txt
 # sudo lxc exec antizapret-vpn -- nano /root/antizapret/config/include-hosts-custom.txt
 # Потом выполните команды для обновления списка антизапрета и очистка кеша днс
-# sudo lxc exec antizapret-vpn -- sh -c "LANG=C.UTF-8 /root/antizapret/doall.sh"
+# sudo lxc exec antizapret-vpn -- sh -c /root/antizapret/doall.sh
 # sudo lxc exec antizapret-vpn -- sh -c "echo 'cache.clear()' | socat - /run/knot-resolver/control/1"
 #
 # Изменить конфигурацию OpenVpn сервера с UDP портом
 # sudo lxc exec antizapret-vpn -- nano /etc/openvpn/server/antizapret.conf
 # Потом перезапустить OpenVpn сервер
 # sudo lxc exec antizapret-vpn -- service openvpn restart
+#
+# Посмотреть статистику подключений OpenVpn (выход Ctrl+X)
+# sudo lxc exec antizapret-vpn -- nano /etc/openvpn/server/logs/status.log -v
 #
 # ====================================================================================================
 #
@@ -122,7 +125,7 @@ sudo lxc exec antizapret-vpn -- sed -i "/\b\(youtube\|youtu\|ytimg\|ggpht\|googl
 sudo lxc exec antizapret-vpn -- sed -i "/\b\(googleusercontent\|cloudfront\|deviantart\)\b/d" /root/antizapret/config/exclude-regexp-dist.awk
 #
 # Обновляем списки антизапрета
-sudo lxc exec antizapret-vpn -- sh -c "LANG=C.UTF-8 /root/antizapret/doall.sh"
+sudo lxc exec antizapret-vpn -- sh -c /root/antizapret/doall.sh
 sudo lxc exec antizapret-vpn -- sh -c "echo 'cache.clear()' | socat - /run/knot-resolver/control/1"
 #
 # Перезапускаем контейнер антизапрета
