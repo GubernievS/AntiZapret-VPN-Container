2d126e47903db3a3e368b034e96decb8575442e4
GubernievS <tyman@ya.ru>
2024-08-07 16:14:07 +0300

    Update antizapret-vpn.sh

--- antizapret-vpn.sh
+++ antizapret-vpn.sh
@@ -3,7 +3,7 @@
 # + Разблокирован YouTube и часть сайтов блокируемых без решения суда
 # Для увеличения скорости используется UDP и 443 порт для обхода блокировки по портам
 #
-# Версия 5.3 от 06.08.2024
+# Версия 5.4 от 07.08.2024
 # https://github.com/GubernievS/AntiZapret-VPN-Container
 #
 # Протестировано на Ubuntu 20.04 - Процессор: 1 core Память: 1 Gb Хранилище: 10 Gb
@@ -94,6 +94,7 @@ sudo lxc exec antizapret-vpn -- sh -c "echo 'youtube.com
 googlevideo.com
 digitalocean.com
 strava.com
+adguard-vpn.com
 tor.eff.org
 news.google.com
 play.google.com
@@ -124,6 +125,11 @@ padlet.com' > /root/antizapret/config/include-hosts-custom.txt"
 # Удаляем исключения из исключений
 sudo lxc exec antizapret-vpn -- sed -i "/\b\(youtube\|youtu\|ytimg\|ggpht\|googleusercontent\|cloudfront\|ftcdn\)\b/d" /root/antizapret/config/exclude-hosts-dist.txt
 sudo lxc exec antizapret-vpn -- sed -i "/\b\(googleusercontent\|cloudfront\|deviantart\)\b/d" /root/antizapret/config/exclude-regexp-dist.awk
+#
+# Добавляем блокирующий рекламу AdGuard DNS
+sudo lxc exec antizapret-vpn -- sh -c "echo '
+policy.add(policy.all(policy.FORWARD({'94.140.14.14'})))
+policy.add(policy.all(policy.FORWARD({'94.140.15.15'})))' > /etc/knot-resolver/kresd.conf"
 #
 # Обновляем списки антизапрета
 sudo lxc exec antizapret-vpn -- sh -c /root/antizapret/doall.sh
